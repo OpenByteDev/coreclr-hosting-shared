e3c4b616163ea9e3fd0a4200ac68de60062c6753
OpenByte <development.openbyte@gmail.com>
2021-09-24 19:26:43 +0200

    Remove UNMANAGED_CALLERS_ONLY_METHOD const

--- src/consts.rs
+++ src/consts.rs
@@ -1,10 +1,4 @@
-use super::{char_t, size_t};
+use super::size_t;
 
 /// The maximum path length on windows platforms ([kind of / not really](https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=cmd)).
 pub const MAX_PATH: size_t = 260;
-
-/// Signifies that the target method is marked with the [`UnmanagedCallersOnlyAttribute`].
-/// This means that the name alone can identify the target method.
-///
-/// [`UnmanagedCallersOnlyAttribute`]: https://docs.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.unmanagedcallersonlyattribute
-pub const UNMANAGED_CALLERS_ONLY_METHOD: *const char_t = usize::MAX as *const _;
